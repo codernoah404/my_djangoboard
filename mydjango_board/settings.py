80e1aef6f017f6ea4521e87a9b6a056045e16604
SKKU-03\User <inhwancho02@skkukdp.re.kr>
2023-10-10 09:36:53 +0900

    settings.py
    실습 컴퓨터로 사용 가능하도록 DB 비밀번호 변경했습니다.

--- mydjango_board/settings.py
+++ mydjango_board/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "django_myboard",
         "USER" : "root",
-        "PASSWORD" : "0128",
+        "PASSWORD" : "0000",
         "HOST" : "127.0.0.1",
         "PORT" : "3306",
     }
